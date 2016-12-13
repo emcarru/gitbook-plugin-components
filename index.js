debce4fd2120584f659d468c9f3596bf753f3f9a
Michael <michaelcarruthers31@gmail.com>
2016-12-13 15:21:27 +0100

    First.

---
+++ index.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var url = require('url');
+
+var urls = [];
+
+module.exports = {
+  // Map of hooks
+  hooks: {
+    "page": function (page) {
+      if (this.output.name != 'website') return page;
+
+      var lang = this.isLanguageBook() ? this.config.values.language : '';
+      if (lang) lang = lang + '/';
+
+      var outputUrl = this.output.toURL('_book/' + lang + page.path);
+
+      urls.push({
+        url: outputUrl + (outputUrl.substr(-5, 5) !== '.html' ? 'index.html' : '')
+      });
+
+      return page;
+    },
+
+    "finish": function() {
+      var $, $el, html;
+      var pathFile = 'docs/layout/header';
+      var tmpl = '';
+
+      if (pathFile && fs.existsSync(pathFile)) tmpl = fs.readFileSync(pathFile, {encoding: 'utf-8'});
+
+      urls.forEach(item => {
+        html = fs.readFileSync(item.url, {encoding: 'utf-8'});
+        $ = cheerio.load(html);
+        $el = $('body .book');
+
+        $el.prepend(tmpl);
+
+        fs.writeFileSync(item.url, $.root().html(), {encoding: 'utf-8'});
+      })
+    }
+  }
+};
